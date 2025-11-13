1e472d59d04822f971fb9391aaecec05648fb406
BISNISPRJCK <bisnisprojeck025@gmail.com>
2025-11-13 08:59:29 +0700

    soalno1 setelah di edit

--- SoalNo1.php
+++ SoalNo1.php
@@ -1,11 +1,24 @@
 <?php
 $jml = $_GET['jml'];
-echo "<table border=1>\n";
+echo "<table border=1 cellpadding=5 cellspacing=0>\n";
+
 for ($a = $jml; $a > 0; $a--) {
-  echo "<tr>\n";
+
+  // 🔹 Hitung total nilai di baris ini
+  $total = 0;
+  for ($b = $a; $b > 0; $b--) {
+    $total += $b;
+  }
+
+  // 🔹 Tampilkan total di baris atas
+  echo "<tr><td colspan='$a' align='center'><b>Total: $total</b></td></tr>\n";
+
+  // 🔹 Tampilkan baris angka
+  echo "<tr>";
   for ($b = $a; $b > 0; $b--) {
     echo "<td>$b</td>";
   }
   echo "</tr>\n";
 }
+
 echo "</table>";
